42e37695b1e6d06d39583e665eb10d1a1c2d94cd
unknown <zeguilherme008@hotmail.com>
2020-10-05 17:08:04 -0300

    Escola

---
+++ Escola/src/main/java/org/generation/Escola/SecretariaApplication.java
@@ -0,0 +1,13 @@
+package org.generation.Escola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretariaApplication.class, args);
+	}
+
+}
---
+++ Escola/src/test/java/org/generation/Escola/SecretariaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.Escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
